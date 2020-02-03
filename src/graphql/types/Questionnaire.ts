a17838f95044f582f7d6dae36b35b469238d30af
Tsuyoshi HARA <chloe463+git@gmail.com>
2020-02-04 02:02:09 +0900

    refactor: Use api client class and data loader

--- src/graphql/types/Questionnaire.ts
+++ src/graphql/types/Questionnaire.ts
@@ -14,11 +14,7 @@ export const Questionnaire = objectType({
       type: "Question",
       list: true,
       nullable: false,
-      resolve: async (root, _args, _context) => {
-        const id = root.id;
-        const res = await axios.get(`http://localhost:3000/questionnaires/${id}/questions`);
-        return res.data;
-      },
+      resolve: async ({ id }, _args, { questionLoader }) => questionLoader.load(id),
     });
   },
 });
--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,31 @@ import express from "express";
 import { ApolloServer } from "apollo-server-express";
 
 import { schema } from "./schema";
+import { QuestionnaireApi } from "./api/questionnaire";
+import { questionLoader } from "./graphql/loaders/questionLoader";
+
+interface Context {
+  questionLoader: ReturnType<typeof questionLoader>;
+  api: {
+    questionnaireApi: QuestionnaireApi,
+  },
+}
+
+const generateContext: () => Context = () => {
+  const questionnaireApi = new QuestionnaireApi();
+  return {
+    questionLoader: questionLoader(questionnaireApi),
+    api: {
+      questionnaireApi,
+    }
+  }
+};
 
 const app = express();
 
 const apolloServer = new ApolloServer({
   schema,
+  context: generateContext(),
 });
 
 apolloServer.applyMiddleware({
